858709c6e1f4693c8a4296a2aa346c3ab11dd959
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-08-24 10:37:54 +0000

    react behind the scene

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,6 +1,15 @@
+import Coffee from "./coffee.jsx";
+
 function App() {
+  const username = "My first React code"
+  
   return (
-    <h1>Hello React with Vite | Aryan Mehta</h1>    
+    <>
+    <h1>Hello React with Vite | Aryan Mehta</h1>
+    <h2>{username}</h2>
+    <Coffee/>
+    <p>taste your coffee</p>
+    </>  
   )
 }
 
---
+++ 01_vite_react/src/coffee.jsx
@@ -0,0 +1,7 @@
+function Coffee(){  // here the name of function must be start with capital letter
+    return(
+        <h3>Coffee is ready</h3>
+    )
+}
+
+export default Coffee
