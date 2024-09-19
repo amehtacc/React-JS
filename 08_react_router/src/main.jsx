b221e4f163d40a2896111e28e4d747d05f4623ca
Aryan Mehta <156931995+amehtacc@users.noreply.github.com>
2024-09-19 10:04:32 +0000

    react router with project

---
+++ 08_react_router/src/main.jsx
@@ -0,0 +1,48 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import { Home, About, Contact, User, Github, Api } from './components/index.js'
+import './index.css'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       }, 
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />      
+      <Route 
+      loader={Api}
+      path='github' element={<Github />} />      
+      <Route path='user/:userid' element={<User />} />      
+    </Route>
+  )
+)
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
